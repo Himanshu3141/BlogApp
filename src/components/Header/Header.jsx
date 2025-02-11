72152ec5e414aa2be1be5378c4729ef08e80eb3d
HIMANSHU. <himanshu3141@gmail.com>
2025-02-12 00:53:52 +0530

    refactor: enhance formatting of navigation items in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,11 +9,32 @@ function Header() {
   const navigate = useNavigate();
 
   const navItems = [
-    { name: 'Home', slug: "/", active: true },
-    { name: "Login", slug: "/login", active: !authStatus },
-    { name: "Signup", slug: "/signup", active: !authStatus },
-    { name: "All Posts", slug: "/all-posts", active: authStatus },
-    { name: "Add Post", slug: "/add-post", active: authStatus },
+    {
+       name: 'Home',
+       slug: "/",
+       active: true
+      
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus
+    },
+    {
+      name:"All Posts",
+      slug: "/all-posts",
+      active: authStatus
+    },
+    {
+      name:"Add Post",
+      slug: "/add-post",
+      active: authStatus
+    },
   ];
 
   return (
