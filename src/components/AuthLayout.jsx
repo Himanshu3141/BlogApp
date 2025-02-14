2f8e5586ccddbcedff754b24736ed035e6d4f7b3
HIMANSHU. <himanshu3141@gmail.com>
2025-02-14 20:04:06 +0530

    update authcomponent

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -10,12 +10,6 @@ export default function Protected({children, authentication = true}) {
 
     useEffect(() => {
 
-        //  if(authStatus===true){
-        //     navigate("/")
-        //   } else if(authStatus===false){
-        //     navigate("/login")
-        //   }
-
         if(authentication && authStatus !== authentication){
             navigate("/login")
         } else if(!authentication && authStatus !== authentication){
